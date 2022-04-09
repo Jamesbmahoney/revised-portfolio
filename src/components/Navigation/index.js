f1f2965f614d80ed9b432c0fa3cb5644e7e3c40c
James Barry <jbm03003@gmail.com>
2022-04-09 17:42:15 -0400

    added styling and image to nav bar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,19 +2,20 @@ import React from "react";
 import { Navbar } from "react-bootstrap";
 import { Container } from "react-bootstrap";
 import { Nav } from "react-bootstrap";
+import selfImage from "../../assets/images/james2.jpg"
 
 function Navigation() {
   return(    
-  <Navbar bg="dark" variant="dark">
-    <Container>
+  <Navbar bg="dark" variant="dark" className="py-0">
+    <img src={selfImage} style={{ width: "3%" }} alt="goofy smile" className="me-3"></img>            
     <Navbar.Brand href="#home">James Mahoney</Navbar.Brand>
+    <Container></Container>
     <Nav className="me-auto">
       <Nav.Link href="#aboutme">About Me</Nav.Link>
       <Nav.Link href="#projects">Projects</Nav.Link>
       <Nav.Link href="#contactinfo">Contact</Nav.Link>
-      <Nav.Link href="#resume">Resume</Nav.Link>
-    </Nav>
-    </Container>
+      <Nav.Link href="#resume">Resume</Nav.Link>      
+    </Nav>        
   </Navbar>  
 
   );
