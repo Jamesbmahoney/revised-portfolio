08ac1d1514282f7d9787e4902322b985dc9746bf
James Barry <jbm03003@gmail.com>
2022-04-10 17:14:03 -0400

    Added an About Me section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import bgImage from "./assets/images/tech_bg.png";
 import Navigation from "./components/Navigation";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-   <div>
-     <Navigation></Navigation>
-   </div>
+    <div style={{ backgroundImage: `url(${bgImage})` }}>
+      <Navigation></Navigation>
+      <main>
+        <About></About>
+        <Contact></Contact>
+        <Footer></Footer>
+      </main>
+    </div>
   );
 }
 
---
+++ src/components/About/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function About() {
+  return (
+    <section class="my-5">
+      <h1 id="about" class="text-white">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Odio euismod lacinia
+        at quis risus sed vulputate. Mattis enim ut tellus elementum sagittis
+        vitae et. Convallis aenean et tortor at risus viverra adipiscing at. Et
+        ultrices neque ornare aenean euismod. Venenatis lectus magna fringilla
+        urna porttitor rhoncus. Tristique senectus et netus et malesuada fames
+        ac turpis egestas. Mus mauris vitae ultricies leo integer malesuada.
+        Tellus molestie nunc non blandit massa. Ut pharetra sit amet aliquam.
+        Morbi enim nunc faucibus a pellentesque sit amet. Volutpat commodo sed
+        egestas egestas fringilla phasellus faucibus. Eleifend quam adipiscing
+        vitae proin sagittis.
+      </h1>
+    </section>
+  );
+}
+
+export default About;
